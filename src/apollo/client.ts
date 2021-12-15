0c369fdf08e48c04e10de882f5bea90c6faf6b62
kimkwanka <kimkwanka@gmail.com>
2021-12-15 22:11:35 +0100

    refactor: extract apollo links

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,87 +1,13 @@
-import {
-  ApolloClient,
-  ApolloLink,
-  InMemoryCache,
-  createHttpLink,
-  from,
-  fromPromise,
-} from '@apollo/client';
-
-import { setContext } from '@apollo/client/link/context';
-
-import { onError } from '@apollo/client/link/error';
-import { accessTokenVar, isRequestPending } from '#apollo/state';
+import { ApolloClient, InMemoryCache, from } from '@apollo/client';
 
 import introspectionResult from '#generated/fragment-matcher';
 
-let tokenRefreshPromise: Promise<void> | null = null;
-
-const silentRefresh = () =>
-  fetch(`${import.meta.env.VITE_MOVIE_API_URL as string}/graphql`, {
-    method: 'POST',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      query: `mutation SilentRefresh {
-        silentRefresh {
-          jwtToken
-          errors {
-            message
-          }
-          statusCode
-        }
-      }`,
-    }),
-  })
-    .then((res) => res.json())
-    .then((json) => {
-      accessTokenVar(json.data.silentRefresh.jwtToken);
-    });
-
-const refreshTokenOnErrorLink = onError(
-  // eslint-disable-next-line consistent-return
-  ({ graphQLErrors, operation, forward }) => {
-    if (
-      graphQLErrors?.some((err) => err.extensions.code === 'UNAUTHENTICATED')
-    ) {
-      if (!tokenRefreshPromise) {
-        tokenRefreshPromise = silentRefresh();
-        tokenRefreshPromise.then(() => {
-          tokenRefreshPromise = null;
-        });
-      }
-
-      return fromPromise(tokenRefreshPromise).flatMap(() => {
-        return forward(operation);
-      });
-    }
-  },
-);
-
-const setPendingRequestLink = new ApolloLink((operation, forward) => {
-  isRequestPending(true);
-
-  return forward(operation).map((response) => {
-    isRequestPending(false);
-    return response;
-  });
-});
-
-const authLink = setContext((_, { headers }) => {
-  return {
-    headers: {
-      ...headers,
-      authorization: `Bearer ${accessTokenVar()}`,
-    },
-  };
-});
-
-const httpLink = createHttpLink({
-  uri: `${import.meta.env.VITE_MOVIE_API_URL as string}/graphql`,
-  credentials: 'include',
-});
+import {
+  authLink,
+  httpLink,
+  refreshTokenOnErrorLink,
+  setPendingRequestLink,
+} from '#apollo/links';
 
 const apolloClient = new ApolloClient({
   cache: new InMemoryCache({
