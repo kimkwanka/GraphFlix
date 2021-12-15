6ecf9dd18164ed693cf856f238efed6c320e1760
kimkwanka <kimkwanka@gmail.com>
2021-12-15 22:07:51 +0100

    feat: enable silent refresh on UNAUTHENTICATED error

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -4,20 +4,68 @@ import {
   InMemoryCache,
   createHttpLink,
   from,
+  fromPromise,
 } from '@apollo/client';
 
 import { setContext } from '@apollo/client/link/context';
 
+import { onError } from '@apollo/client/link/error';
 import { accessTokenVar, isRequestPending } from '#apollo/state';
 
 import introspectionResult from '#generated/fragment-matcher';
 
+let tokenRefreshPromise: Promise<void> | null = null;
+
+const silentRefresh = () =>
+  fetch(`${import.meta.env.VITE_MOVIE_API_URL as string}/graphql`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `mutation SilentRefresh {
+        silentRefresh {
+          jwtToken
+          errors {
+            message
+          }
+          statusCode
+        }
+      }`,
+    }),
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      accessTokenVar(json.data.silentRefresh.jwtToken);
+    });
+
+const refreshTokenOnErrorLink = onError(
+  // eslint-disable-next-line consistent-return
+  ({ graphQLErrors, operation, forward }) => {
+    if (
+      graphQLErrors?.some((err) => err.extensions.code === 'UNAUTHENTICATED')
+    ) {
+      if (!tokenRefreshPromise) {
+        tokenRefreshPromise = silentRefresh();
+        tokenRefreshPromise.then(() => {
+          tokenRefreshPromise = null;
+        });
+      }
+
+      return fromPromise(tokenRefreshPromise).flatMap(() => {
+        return forward(operation);
+      });
+    }
+  },
+);
+
 const setPendingRequestLink = new ApolloLink((operation, forward) => {
   isRequestPending(true);
 
-  return forward(operation).map((data) => {
+  return forward(operation).map((response) => {
     isRequestPending(false);
-    return data;
+    return response;
   });
 });
 
@@ -39,7 +87,12 @@ const apolloClient = new ApolloClient({
   cache: new InMemoryCache({
     possibleTypes: introspectionResult.possibleTypes,
   }),
-  link: from([authLink, setPendingRequestLink, httpLink]),
+  link: from([
+    refreshTokenOnErrorLink,
+    authLink,
+    setPendingRequestLink,
+    httpLink,
+  ]),
 });
 
 export default apolloClient;
