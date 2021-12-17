c49d89e2b5993ac8e0f143fe87081e36cc6a2bb5
kimkwanka <kimkwanka@gmail.com>
2021-12-17 23:57:48 +0100

    fix: correct tsconfig and jest config paths

--- jest.config.js
+++ jest.config.js
@@ -9,10 +9,11 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
-    '^#components/(.*)$': '<rootDir>//src/components/$1',
-    '^#redux/(.*)$': '<rootDir>/src/redux/$1',
-    '^#hooks/(.*)$': '<rootDir>//src/hooks/$1',
-    '^#views/(.*)$': '<rootDir>//src/views/$1',
-    "\\.(css|scss)$": "<rootDir>/jest/style.mock.ts"
+    '^#apollo/(.*)$': '<rootDir>/src/apollo/$1',
+    '^#components/(.*)$': '<rootDir>/src/components/$1',
+    '^#generated/(.*)$': '<rootDir>/src/_generated/$1',
+    '^#hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^#views/(.*)$': '<rootDir>/src/views/$1',
+    '\\.(css|scss)$': '<rootDir>/jest/style.mock.ts',
   },
 };
